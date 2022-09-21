2d98fbc7134736512544f8e55c642a5ffd3b6f32
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 11:08:08 +0200

    add modules: templates, clients, ui, orders, ...

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  public title: string | undefined; // on peut definir deus types pour une propriété
+  public title!: string | undefined; // on peut definir deus types pour une propriété
+  // public title!: pour la valeur par defaut undefined
   // ou public title: string = 'crm'; si pas de constructeur
 
   constructor() {
