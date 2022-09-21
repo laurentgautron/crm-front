c39bfaf7868c2e2c7836c9c3a2b80f3a254a1440
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 12:00:35 +0200

    add views

--- src/app/clients/clients.module.ts
+++ src/app/clients/clients.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ClientsRoutingModule } from './clients-routing.module';
+import { PageListClientsComponent } from './views/page-list-clients/page-list-clients.component';
+import { PageAddClientComponent } from './views/page-add-client/page-add-client.component';
+import { PageEditClientComponent } from './views/page-edit-client/page-edit-client.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListClientsComponent,
+    PageAddClientComponent,
+    PageEditClientComponent
+  ],
   imports: [
     CommonModule,
     ClientsRoutingModule
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -2,10 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { PageListOrdersComponent } from './views/page-list-orders/page-list-orders.component';
+import { PageAddOrderComponent } from './views/page-add-order/page-add-order.component';
+import { PageEditOrderComponent } from './views/page-edit-order/page-edit-order.component';
+import { PageNotFoundComponent } from './views/page-not-found/page-not-found.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListOrdersComponent,
+    PageAddOrderComponent,
+    PageEditOrderComponent,
+    PageNotFoundComponent
+  ],
   imports: [
     CommonModule,
     OrdersRoutingModule
