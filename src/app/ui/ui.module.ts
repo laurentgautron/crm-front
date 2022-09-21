da893707d6ef9486f8f79f77dc9a30c42b5cad74
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 15:12:13 +0200

    stylings and ui2

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,12 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { Ui2Component } from './component/ui2/ui2.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  declarations: [Ui2Component],
+  imports: [CommonModule],
+  exports: [Ui2Component],
 })
-export class UiModule { }
+export class UiModule {}
