daf518a6eeb53b1e6321857dc9bc12c5129a505d
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 15:24:20 +0200

    add html css for ui

--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ui2Component } from './component/ui2/ui2.component';
+import { UiComponent } from './component/ui/ui.component';
 
 @NgModule({
-  declarations: [Ui2Component],
+  declarations: [Ui2Component, UiComponent],
   imports: [CommonModule],
-  exports: [Ui2Component],
+  exports: [Ui2Component, UiComponent],
 })
 export class UiModule {}
