285c71b5ddf6c574fb190b93f2429d40d3669984
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 11:40:32 +0200

    export modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  // on n'importe pas les modules ici mais dans le core
+  // optimisation du code : un role pour un module
+  imports: [BrowserModule, AppRoutingModule, CoreModule],
   providers: [],
   bootstrap: [AppComponent], // pour démarrer l'application
 })
-export class AppModule {}
+export class AppModule {
+  // acces aux imports et exports ( exports des modules importés: ici CoreModule)
+  // le role d'appModule est de démarrer l'application avec les modules au démarrage
+}
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-
-
+import { IconsModule } from '../icons/icons.module';
+import { LoginModule } from '../login/login.module';
+import { TemplatesModule } from '../templates/templates.module';
+import { UiModule } from '../ui/ui.module';
 
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule],
+  exports: [IconsModule, LoginModule, TemplatesModule, UiModule],
 })
-export class CoreModule { }
+export class CoreModule {
+  // acces aux imports du module core
+}
