6c86254f9dbb025e5bf04d6f29fa384f97b07eb1
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 10:13:18 +0200

    first component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'crm';
+  public title: string | undefined; // on peut definir deus types pour une propriété
+  // ou public title: string = 'crm'; si pas de constructeur
+
+  constructor() {
+    this.title = 'crm';
+  }
 }
