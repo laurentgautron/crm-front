4587699c4b78c31f6aee74bf79e71953590aed94
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-21 16:25:36 +0200

    add transfusion and class

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,11 +4,22 @@ import { IconsModule } from '../icons/icons.module';
 import { LoginModule } from '../login/login.module';
 import { TemplatesModule } from '../templates/templates.module';
 import { UiModule } from '../ui/ui.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
   imports: [CommonModule],
-  exports: [IconsModule, LoginModule, TemplatesModule, UiModule],
+  exports: [
+    IconsModule,
+    LoginModule,
+    TemplatesModule,
+    UiModule,
+    HeaderComponent,
+    NavComponent,
+    FooterComponent,
+  ],
 })
 export class CoreModule {
   // acces aux imports du module core
--- src/app/ui/ui.module.ts
+++ src/app/ui/ui.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Ui2Component } from './component/ui2/ui2.component';
 import { UiComponent } from './component/ui/ui.component';
+import { CoreModule } from '../core/core.module';
 
 @NgModule({
   declarations: [Ui2Component, UiComponent],
