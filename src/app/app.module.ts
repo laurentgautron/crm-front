487791cf5f7bbb5ace7b1bf8863c548f155e2866
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-27 10:27:37 +0200

    add currency pipe

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
-import { NgModule } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 import { HttpClientModule } from '@angular/common/http';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr';
+registerLocaleData(localeFr);
 
 @NgModule({
   declarations: [AppComponent],
@@ -12,7 +15,11 @@ import { HttpClientModule } from '@angular/common/http';
   // optimisation du code : un role pour un module
   // CoreModule avant ApproutingModule pour les imports de login
   imports: [BrowserModule, CoreModule, AppRoutingModule, HttpClientModule],
-  providers: [],
+  // pour mettre par défaut dans tous les pipes currency de l'application
+  providers: [
+    { provide: DEFAULT_CURRENCY_CODE, useValue: 'EUR' },
+    { provide: LOCALE_ID, useValue: 'fr-FR' },
+  ],
   bootstrap: [AppComponent], // pour démarrer l'application
 })
 export class AppModule {
