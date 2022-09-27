b6a5b8ca4c6a5c83f8b6212a756fd44c96f0ab18
laurent GAUTRON <laurentgautron@protonmail.com>
2022-09-27 11:49:10 +0200

    can chage state on json base

--- src/app/orders/views/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/views/page-list-orders/page-list-orders.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+import { StateOrder } from 'src/app/core/enums/state-order';
 import { Order } from 'src/app/core/models/order';
 import { OrdersService } from '../../services/orders.service';
 
@@ -13,10 +14,12 @@ export class PageListOrdersComponent implements OnInit {
   public title: string;
   public headers: string[];
   public collection$: Observable<Order[]>;
+  public states: string[];
 
   constructor(private ordersService: OrdersService) {
     this.title = 'list of orders';
     this.collection$ = this.ordersService.collection$;
+    this.states = Object.values(StateOrder);
     this.headers = [
       'Type',
       'Client',
@@ -39,6 +42,14 @@ export class PageListOrdersComponent implements OnInit {
 
   public changeTitle(): void {
     this.title = 'title changed';
+  }
+
+  public changeState(item: Order, event: any) {
+    const state = event.target.value;
+    this.ordersService.changeState(item, state).subscribe((data) => {
+      // on change les valeurs du item qui sont à changer: Object.assign = bonne copie
+      Object.assign(item, data);
+    });
   }
 
   // en typescript il n'y a que des number, pas de float, double, ...
